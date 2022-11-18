89421a1bff8f0b08a97583b8d9bd0ffe6f90f342
K4zoku <kazoku0356@gmail.com>
2022-11-18 19:08:44 +0700

    Added snowflake-hibernate-type module

---
+++ snowflake-hibernate-type/src/main/java/io/gitlab/k4zoku/snowflake/hibernate/SnowflakeTypeDescriptor.java
@@ -0,0 +1,62 @@
+package io.gitlab.k4zoku.snowflake.hibernate;
+
+import io.gitlab.k4zoku.snowflake.Snowflake;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.descriptor.java.AbstractTypeDescriptor;
+
+public class SnowflakeTypeDescriptor extends AbstractTypeDescriptor<Snowflake> {
+
+    public static final SnowflakeTypeDescriptor INSTANCE = new SnowflakeTypeDescriptor();
+
+    private SnowflakeTypeDescriptor() {
+        super(Snowflake.class);
+    }
+
+    @Override
+    public String toString(Snowflake value) {
+        return value.toString();
+    }
+
+    @Override
+    public Snowflake fromString(String string) {
+        return Snowflake.of(string);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <X> X unwrap(Snowflake value, Class<X> type, WrapperOptions options) {
+        if (value == null) {
+            return null;
+        }
+        if (Snowflake.class.isAssignableFrom(type)) {
+            return (X) value;
+        }
+        if (long.class.isAssignableFrom(type) || Long.class.isAssignableFrom(type)) {
+            return (X) Long.valueOf(value.getAsLong());
+        }
+        if (String.class.isAssignableFrom(type)) {
+            return (X) value.toString();
+        }
+        throw unknownUnwrap(type);
+    }
+
+    @Override
+    public <X> Snowflake wrap(X value, WrapperOptions options) {
+        if (value == null) {
+            return null;
+        }
+        if (long.class.isAssignableFrom(value.getClass())) {
+            return Snowflake.of((long) value);
+        }
+        if (Long.class.isAssignableFrom(value.getClass())) {
+            return Snowflake.of((Long) value);
+        }
+        if (String.class.isAssignableFrom(value.getClass())) {
+            return Snowflake.of((String) value);
+        }
+        if (Snowflake.class.isAssignableFrom(value.getClass())) {
+            return (Snowflake) value;
+        }
+        throw unknownWrap(value.getClass());
+    }
+}
