e471e1de04353595e1f07034cca48dbefb2a3f5e
K4zoku <kazoku0356@gmail.com>
2022-11-20 20:08:22 +0700

    Added benchmark

--- src/test/java/io/gitlab/k4zoku/snowflake/SnowflakeGeneratorPoolBenchmark.java
+++ src/test/java/io/gitlab/k4zoku/snowflake/SnowflakeGeneratorPoolBenchmark.java
@@ -23,12 +23,23 @@ public class SnowflakeGeneratorPoolBenchmark {
     @Warmup(iterations = 1)
     @Measurement(iterations = 1)
     @Threads(4)
-    public void generateBenchmark() {
+    public void snowflakeGenerateBenchmarkMultiThread() {
+        pool.generate();
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    @OutputTimeUnit(TimeUnit.MILLISECONDS)
+    @Fork(value = 1, warmups = 1)
+    @Warmup(iterations = 1)
+    @Measurement(iterations = 1)
+    public void snowflakeGenerateBenchmarkSingleThread() {
         pool.generate();
     }
 
     @TearDown
     public void tearDown() {
-        System.out.println(pool.getGeneratedCount());
+        System.out.println();
+        System.out.println("Total snowflake generated: " + pool.getGeneratedCount());
     }
 }
